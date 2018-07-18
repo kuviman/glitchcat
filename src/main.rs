72b27b7e77f2f9f72fc6b517344d65695249134b
kuviman <kuviman@gmail.com>
2018-07-18 20:46:07 +0400

    Parse homoglyphs file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
 extern crate console;
 
+use std::collections::{HashMap, HashSet};
+
+struct Homoglyphs {
+    groups: Vec<HashSet<char>>,
+    group_map: HashMap<char, usize>,
+}
+
+impl Homoglyphs {
+    pub fn new() -> Self {
+        let mut groups = Vec::new();
+        let mut group_map = HashMap::new();
+        for line in include_str!("homoglyphs.txt").lines() {
+            if line.starts_with('#') {
+                continue;
+            }
+            let group = line.chars().collect();
+            for &c in &group {
+                group_map.insert(c, groups.len());
+            }
+            groups.push(group);
+        }
+        Self { groups, group_map }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let homoglyphs = Homoglyphs::new();
+    for c in "Hello, world!".chars() {
+        println!("{:?}: {:?}", c, homoglyphs.groups[homoglyphs.group_map[&c]]);
+    }
 }
